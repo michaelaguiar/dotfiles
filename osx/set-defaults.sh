a2e349bc3fd68acdc1e2bdbbd9af0b14530c3ae3
Michael Aguiar <michaelaguiar@me.com>
2011-11-16 11:52:38 -0700

    OS X Defaults

---
+++ osx/set-defaults.sh
@@ -0,0 +1,9 @@
+# Sets OS X defaults
+#
+# Run ./set-defaults.sh to apply
+
+# Use AirDrop over every interface
+defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
+
+# Always open Finder in list view
+defaults write com.apple.Finder FXPreferredViewStyle Nlsv
